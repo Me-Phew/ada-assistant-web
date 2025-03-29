9a46770b7717b47a9f545659736b4db1c0587fa4
Marcin <marcdud3k@gmail.com>
2025-03-29 19:09:40 +0100

    feat: add responsiveness for HomeHero

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -21,10 +21,24 @@
     margin-top: 12rem;
     grid-column: 2/5;
     color: $color_text_primary;
+
+    @include tablet-and-below {
+      grid-column: 2/12;
+      margin-top: 8rem;
+      text-align: center;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
   }
 
   &__title {
     @include h1;
+
+    @include mobile {
+      font-size: 3.6rem;
+      line-height: 1.3;
+    }
   }
 
   &__subtitle {
@@ -35,6 +49,11 @@
 
   &__cta-button {
     margin-top: 3.2rem;
+
+    @include mobile {
+      width: 100%;
+      max-width: none;
+    }
   }
 }
 </style>
