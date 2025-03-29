175e55f34aa782bd1ad8d3b3417fb7bd4ca0f2ba
Marcin <marcdud3k@gmail.com>
2025-03-29 21:24:07 +0100

    feat: add custom layot for a login page

---
+++ layouts/custom.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="layout-custom">
+    <slot />
+    <BaseFooter />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout-custom {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+</style>
