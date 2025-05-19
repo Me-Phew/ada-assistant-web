f4da5cf49801f095581c22d6fc023f27bd792e11
Marcin <marcdud3k@gmail.com>
2025-05-19 23:12:40 +0200

    feat: fix route to spotify callback

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,7 +4,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     `[Auth Global Middleware] Path: ${to.fullPath}. Customer state: ${JSON.stringify(customer.value)}`,
   );
 
-  const publicPages = ["/", "/login", "/register", "/forgot-password"];
+  const publicPages = ["/", "/login", "/register", "/forgot-password", "/spotifycallback"];
   const isPublicPage = publicPages.includes(to.path) || to.path.startsWith("/reset-password");
 
   if (!customer.value) {
@@ -16,7 +16,12 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     }
     console.log("[Auth Global Middleware] No customer, but path is public. Allowing.");
   } else {
-    if (isPublicPage && to.path !== "/" && to.path !== "/reset-password") {
+    if (
+      isPublicPage &&
+      to.path !== "/" &&
+      to.path !== "/reset-password" &&
+      to.path !== "/spotifycallback"
+    ) {
       console.log(
         `[Auth Global Middleware] Authenticated user on a public page (${to.path}). Redirecting to appropriate dashboard.`,
       );
